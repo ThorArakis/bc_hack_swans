555142ff68cfa078e769c44f2ab62fda6f1f3125
Ryan Debenham <rdebenham@backcountry.com>
2013-03-23 00:37:02 -0600

    need to merge

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 
-/*
- * GET home page.
- */
+search = require('../services/search');
 
 exports.index = function(req, res){
   //res.render('index', { layout: false, title: 'The Outfitter' });
@@ -13,3 +11,22 @@ exports.partials = function(req, res){
 	var name = req.params.name;
 	res.render('partials/' + name);
 };
+
+exports.getProducts = function(req, res) {
+    console.log(JSON.stringify(req.query));
+    var options = req.query;
+    if(options.category) {
+        console.log(JSON.stringify(options));
+        search.performSearch(options, function(err, products) {
+            if(!err) {
+                res.send(products);
+            } else {
+                console.log("hello")
+                throw err;
+            }   
+        }); 
+    } else {
+        throw new Error("Category is required for product search");
+    }   
+};
+~ 
