5a60a78f09bbb860018877b4efa9b36c3bee885f
Silvia Charpentier <scharpentier@backcountry.com>
2013-03-23 02:29:24 -0600

    Adding second carousel method.

--- public/javascript/controllers/home.js
+++ public/javascript/controllers/home.js
@@ -4,9 +4,15 @@ function HomeCtrl($scope, $http) {
     var brand = "marmot";
     var gender = "mens";
     
+    var category2 = "pants";
+    
       $http.get("/api/products?category=" + category + "&brand=" + brand + "&gender=" + gender).success(function(response) {
         $scope.upperProducts = response;
       });
+     
+      $http.get("/api/products?category=" + category2 + "&brand=" + brand + "&gender=" + gender).success(function(response) {
+         $scope.lowerProducts = response;
+       });
     
     $(document).ready(function(){
 	    $('#upperCarousel, #lowerCarousel').carousel({ interval: false });		
