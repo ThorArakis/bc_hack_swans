25d8549279b1c8ef8c82498cdbbc87001d45384e
Chris Stephens <cstephens@backcountry.com>
2013-03-22 22:36:46 -0600

    adding chris view/controller

---
+++ public/javascript/controllers/chris.js
@@ -0,0 +1,10 @@
+function ChrisCtrl($scope) {
+	var fs = require('fs');
+	$scope.imageUrl = "www.backcountry.com/images/items/large/ARC/ARC1180/WHE.jpg";
+
+	$scope.clickFunction = function() {
+		alert('hey');
+	}
+
+
+}
