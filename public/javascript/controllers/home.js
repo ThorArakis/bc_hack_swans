f5551cb12589c68c9f35a97513646f6a0104006e
Chris Stephens <cstephens@backcountry.com>
2013-03-23 04:24:19 -0600

    removing old images. establishing the image caching

--- public/javascript/controllers/home.js
+++ public/javascript/controllers/home.js
@@ -6,12 +6,15 @@ function HomeCtrl($scope, $http) {
     
     var category2 = "pants";
     
-      $http.get("/api/products?category=" + category + "&brand=" + brand + "&gender=" + gender).success(function(response) {
-        $scope.upperProducts = response;
+      $http.get("/api/products?category=" + category + "&brand=" + brand + "&gender=" + gender)
+				.success(function(response) {
+        	$scope.upperProducts = response;
       });
      
-      $http.get("/api/products?category=" + category2 + "&brand=" + brand + "&gender=" + gender).success(function(response) {
-         $scope.lowerProducts = response;
+      $http.get("/api/products?category=" + category2 + "&brand=" + brand + "&gender=" + gender)
+				.success(function(response) {
+					$scope.lowerProducts = response;
+					
        });
     
     $(document).ready(function(){
--- services/image_serve.js
+++ services/image_serve.js
@@ -1,18 +1,50 @@
 var fs = require('node-fs');
+var sys = require('sys');
 var http = require('http');
 var urllib = require('url');
 var exec = require('child_process').exec;
 
 exports.getImage = function(options, callback) {
-	console.log("getting url: " + options.url);
-	parsedUrl = urllib.parse(options.url);
+	var splitUrl = options.url.split('/');
+	var fileName = "public/images/cached/" + splitUrl[splitUrl.length-2] + "_" + splitUrl[splitUrl.length-1].split('.')[0] + ".gif";
+
+	fs.exists(fileName, function(exists){
+		if (exists){
+			getLocalImage(fileName, callback);
+		}
+		else {
+			getRemoteImage(options.url, fileName, callback);
+		}
+	});
+}
+
+var getLocalImage = function(fileName, callback) {
+	console.log("reading file: " + fileName);
+	fs.readFile(fileName, encoding='binary', callback);
+}
+
+var transformImage = function(fileName, callback) {
+	function puts(error, stdout, stderr) {
+		if (error) {
+			console.log("error: " + error);
+			callback(error, [])
+		}
+		else {
+			getLocalImage(fileName, options.callback);
+		}
+	}
+	var convertCmd = "convert " + fileName  + " -fuzz 10% -transparent white " + fileName;
+	var options = {};
+	options.callback = callback;
+	exec(convertCmd , options, puts);
+}
+
+var getRemoteImage = function(url, fileName, callback) {
+	console.log("getting url: " + url);
+	parsedUrl = urllib.parse(url);
 	if (!parsedUrl.hostname || !parsedUrl.pathname){
 		console.log("invalid url specified for image!");
 	}
-
-	var splitUrl = options.url.split('/');
-	var fileName = splitUrl[splitUrl.length-2] + "_" + splitUrl[splitUrl.length-1];
-	console.log("file name: " + fileName);
 
 	var httpOptions = {
 		host: parsedUrl.hostname,
@@ -21,14 +53,13 @@ exports.getImage = function(options, callback) {
 	};
 
 	http.get(httpOptions, function(res) {
-		console.log("Got respose: " + res.statusCode);
 		res.setEncoding('binary')
 		var imagedata = ''
 		res.on('data', function(chunk){
 			imagedata+= chunk;
 		});
 		res.on('end', function(){
-			fs.writeFile('public/images/cached/new_image.jpg', imagedata, 'binary', callback(null, imagedata));
+			fs.writeFile(fileName, imagedata, 'binary', transformImage(fileName, callback));
 		});
 	}).on('error', function(e) {
 		console.log("Error getting the image. Error: " + e.message);
