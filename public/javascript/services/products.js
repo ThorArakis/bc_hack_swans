f82481cd6a00cd39c85e9ece886fd4b06f4ac236
Silvia Charpentier <scharpentier@backcountry.com>
2013-03-23 01:16:42 -0600

    Comsuming products api and binding it with the upperproducts carousel.

---
+++ public/javascript/services/products.js
@@ -0,0 +1,20 @@
+app.service("productService", function($rootScope) {
+    var self = this;
+    
+    this.getUpperProducts = function(category, brand, gender, callback) {
+        //TODO ADD UPPER KEYWORDS
+        $.ajax({
+            type: "get",
+            url: "api/products/r",
+            data: {category : category, brand : brand, gender : gender},            
+            statusCode: {
+                    404: function(xhr) {
+                        callback(new Error("There are no products for this search"));
+                    }
+                },
+            success: function(products) {
+                    if(typeof(callback) == "function") callback(null, products);
+            }
+        });
+    };
+});
