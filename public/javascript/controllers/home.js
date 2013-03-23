b10c94ac213d60d1fa5fa80718b33ebc72cf3f45
Silvia Charpentier <scharpentier@backcountry.com>
2013-03-23 04:41:06 -0600

    Adding filters dropdowns.

--- public/javascript/controllers/home.js
+++ public/javascript/controllers/home.js
@@ -13,6 +13,50 @@ function HomeCtrl($scope, $http) {
       $http.get("/api/products?category=" + category2 + "&brand=" + brand + "&gender=" + gender).success(function(response) {
          $scope.lowerProducts = response;
        });
+     
+     $scope.brands = [
+        { name: 'The North Face' },
+        { name: 'Marmot' },
+        { name: 'Patagonia' }      
+      ];
+      
+      $scope.categories = [
+		{ name: 'Hoodies & Sweatshirts' },
+	    { name: 'Snowboard Jackets' },
+        { name: 'Snowboard Jackets' },
+        { name: 'Bike Jackets' },
+        { name: 'Down Jackets' },
+        { name: 'Ski Jackets' },
+        { name: 'Softshell Jackets' },
+        { name: 'Fleece Jackets' },
+        { name: 'Casual Jackets' },
+        { name: 'Jackets' }
+      ];
+      
+      $scope.genders = [
+            { name: 'Men' },
+            { name: 'Women' },
+            { name: 'Boys' },
+            { name: 'Girls' }               
+          ];
+     
+    $scope.colors = [
+          { name: 'White' },
+          { name: 'Black' },
+          { name: 'Blue' },
+          { name: 'Green' },
+          { name: 'Red' },
+          { name: 'Yellow' }                    
+    ];
+    
+    $scope.sizes = [
+             { name: 'XS' },
+             { name: 'S' },
+             { name: 'M' },
+             { name: 'L' },
+             { name: 'XL' },
+             { name: 'XXL' }                    
+       ];
     
     $(document).ready(function(){
 	    $('#upperCarousel, #lowerCarousel').carousel({ interval: false });		
